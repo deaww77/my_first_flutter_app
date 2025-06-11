6646e774582d297c44179e7a59458ad93ff1033f
deaww77 <126137481+deaww77@users.noreply.github.com>
2025-06-11 11:50:50 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter/my_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 15, 233, 95)),
+      ),
+      home: const MyHomePage(title: 'chanon app'),
+    );
+  }
+}
